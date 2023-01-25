c30366285ff850db389c27def570e92436ed4511
Miguel Andres Jurado Negrete <miguel.jurado@asteci.com.mx>
2023-01-24 23:17:29 -0600

    REFACTOR: store implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import { authReducer } from '../reducers';
 
 const isDevEnviroment = true; //TODO: Implements env
 
-const store = configureStore({
-  reducer: {
-    'auth': authReducer
-  },
+const rootReducers = combineReducers({
+  'auth': authReducer,
+})
+
+export const setupStore = ( preloadedState?: PreloadedState<RootState> ) => configureStore({
+  reducer: rootReducers,
   devTools: isDevEnviroment,
+  preloadedState,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type GetRootState = () => RootState;
+const store = setupStore();
 
+export type RootState = ReturnType<typeof rootReducers>;
+export type GetRootState = () => RootState;
 
 export default store;
 
