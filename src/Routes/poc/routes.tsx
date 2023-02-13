2c759b3c1edee9ab9b990a448619fbcd356ed4a0
Miguel Andres Jurado Negrete <miguel.jurado@asteci.com.mx>
2023-02-13 12:37:17 -0600

    ADD: Route descriptor

--- src/Routes/poc/routes.tsx
+++ src/Routes/poc/routes.tsx
@@ -1,3 +1,50 @@
 import React from 'react';
+import { Login, RecoverPassword, Register } from '../../pages/auth';
+import { RecordRoute, Routes } from './CreateRoutes.poc';
+import { LayoutProtected } from './LayoutProtected';
+import { UnprotectedLayout } from './UnprotectedLayout';
 
-export const routes = []
+export const protectedRoute: RecordRoute = {
+  'protected': {
+    component: () => <LayoutProtected />,
+    resolve: () => {},
+    path: 'home',
+    index: true
+  },
+  'noprotected': {
+    component: () => <UnprotectedLayout />,
+    resolve: () => {},
+    path: 'auth',
+    index: true
+  },
+  '404-not-found': {
+    component: () => <h1> Not Found </h1>,
+    resolve: () => {},
+    path: '*',
+    index: true
+  }
+};
+
+export const routes: Routes = [
+  {
+    component: () => <Login />,
+    path: 'login',
+    protectorType: 'noprotected'
+  },
+  {
+    component: () => <Register />,
+    path: 'register',
+    protectorType: 'noprotected'
+  },
+  {
+    component: () => <RecoverPassword />,
+    path: 'recover-password',
+    protectorType: 'noprotected'
+  },
+  {
+    component: () => <h2> HOME </h2>,
+    path: '',
+    protectorType: 'protected'
+  },
+];
+
