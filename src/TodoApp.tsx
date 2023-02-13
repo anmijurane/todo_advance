0b6505ebbc21cad474d4058c9a4eeabac6b35727
Miguel Andres Jurado Negrete <miguel.jurado@asteci.com.mx>
2023-02-13 14:29:33 -0600

    REFACTOR: implementation of routing by descriptors.

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,27 +1,18 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
-import { createRoutes, PathRoutes } from './Routes/CreateRoutes';
+
 import store from './store';
+import { RenderRoutes } from './Routes/CreateRoutes';
+import { protectedRoute, routes } from './Routes/routes';
 
 export const TodoApp = () => {
-
-  const routes = useMemo(() => createRoutes(), []);
 
   return (
     <Provider store={store}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          {Object.keys(routes).map((route) => {
-            //V1 - implements Routes with Descriptor
-            //TODO: V2 - implements Component with Route Protected
-            const Component = routes[route as PathRoutes].component;
-            return <Route key={route} path={route} element={<Component />} />
-          })}
-        </Routes>
-      </BrowserRouter>
+      {RenderRoutes( protectedRoute, routes )}
     </Provider>
-  )
+  );
+
 };
